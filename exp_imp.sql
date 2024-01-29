5ceb59f8b19ad9d2921a4f3b714cff4f79514f8b
Yunita Darmastuti <114557576+nitnot23@users.noreply.github.com>
2024-01-29 15:55:39 +0700

    exp_imp.sql

--- exp_imp.sql
+++ exp_imp.sql
@@ -1,3 +1,31 @@
+-- Export Table Using Oracle Data Pump --
+
+DECLARE
+  v_hdnl NUMBER;
+BEGIN
+  v_hdnl := DBMS_DATAPUMP.OPEN(
+    operation => 'EXPORT', 
+    job_mode  => 'TABLE', 
+    job_name  => null
+  );
+  DBMS_DATAPUMP.ADD_FILE( 
+    handle    => v_hdnl, 
+    filename  => 'WFASSIGNEMNTS_WFCALLSTjACK_29012024.dmp', 
+    directory => 'DATA_PUMP_DIR', 
+    filetype  => dbms_datapump.ku$_file_type_dump_file
+  );
+  DBMS_DATAPUMP.ADD_FILE( 
+    handle    => v_hdnl, 
+    filename  => 'WFASSIGNEMNTS_WFCALLSTACK_29012024_exp.log', 
+    directory => 'DATA_PUMP_DIR' , 
+    filetype  => dbms_datapump.ku$_file_type_log_file
+  );
+  DBMS_DATAPUMP.METADATA_FILTER(v_hdnl,name => 'SCHEMA_LIST', value => '''MAXPRD''');
+  DBMS_DATAPUMP.METADATA_FILTER(v_hdnl,name => 'NAME_LIST', value => '''WFASSIGNEMNTS'',''WFCALLSTACK''');
+  DBMS_DATAPUMP.START_JOB(v_hdnl);
+END;
+/
+
 -- Export Schema Using Oracle Data Pump --
 
 DECLARE
